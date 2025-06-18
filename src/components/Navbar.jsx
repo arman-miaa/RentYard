8e0637c81068423fbd4aade0591e6316f5b0d76d
Arman <mdarman365506@gmail.com>
2025-06-19 05:50:20 +0600

    Add Navbar component and layout setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import logo from "../assets/logo.jpg"
+
+const Navbar = () => {
+  return (
+    <div className=" border-b border-base-300">
+      <div className="navbar container mx-auto">
+        <div className="flex-1">
+          <img className=" text-xl cursor-pointer" src={logo} alt="logo" />
+        </div>
+        <div className="flex-none">
+          <button className="btn btn-xs">Exit</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar
